91b0ecc25f62715a561e2743bce2e3ba69435c7f
Waqaas Aslam <aslamw@mymacewan.ca>
2017-05-18 03:46:39 -0600

    Categorize stock tickers, seed database with stock tickers

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Crockett\CsvSeeder\CsvSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('tickers')->delete();
+        
+        $this->call(TickersTableSeeder::class);
     }
 }

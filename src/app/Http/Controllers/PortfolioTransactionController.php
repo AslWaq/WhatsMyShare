3d12317a81cd002b3f37c76e7960cdf74dd2239c
Waqaas Aslam <aslamw@mymacewan.ca>
2017-06-12 23:10:10 -0600

    Short stocks function

--- src/app/Http/Controllers/PortfolioTransactionController.php
+++ src/app/Http/Controllers/PortfolioTransactionController.php
@@ -66,12 +66,11 @@ class PortfolioTransactionController extends Controller
     }elseif($stock->shares == $ar[1]){
       $user->cash += ($ar[1] * $ar[2]);
       //invest score change = (new price * shares sold) - (old price * shares sold)
-      $user->invest_score += ($ar[1] * $ar[2]) - (($stock->price) * $ar[1]);
+      $user->invest_score += ($ar[1] * $ar[2]);
       $user->save();
       $stock->delete();
     }else{
       $stock->shares -= $ar[1];
-      $user->invest_score += ($ar[1] * $ar[2]) - (($stock->price) * $ar[1]);
       $stock->price = $ar[2];
       $stock->initial_val = ($stock->shares) * ($stock->price);
       $stock->save();
@@ -80,6 +79,54 @@ class PortfolioTransactionController extends Controller
       $user->invest_score += ($ar[1] * $ar[2]);
       $user->save();
     }
+  }
+
+  //Short Stock
+  public function getShort(Request $request){
+    $user = Auth::user();
+    $dt = Carbon::now()->format('Y-m-d');
+    $ar = json_decode($request->order);
+    if ($datshort = Short::where('user_id',$user->id)->where('stock_ticker',$ar[0])->first() == null){
+    $short = new Short;
+    $short->user_id = Auth::user()->id;
+    $short->stock_ticker = $ar[0];
+    $short->shares = $ar[1];
+    $short->initial_price = $ar[2];
+    $short->shorted_at = $dt;
+    $short->save();
+
+    $cartArray = json_decode($user->shopping_cart);
+    for ($x=0; $x < count($cartArray); $x++){
+      if (json_decode($cartArray[$x])[0] == $ar[0]){
+        $y = $x;
+      }
+    }
+    unset($cartArray[$y]);
+    $cart = json_encode(array_values($cartArray));
+    $user->shopping_cart = $cart;
+    $user->save();
+  }else{
+    return view('dashboard'); //send back message saying you already have this stock shorted
+  }
+
+
+
+    return view('dashboard');
+  }
+
+  //pay the Short back
+  public function buyShort(Request $request){
+    //buy the shares, lose cash. but invest score may still go up if you bought at lower price than you shorted
+    $user = Auth::user();
+    $ar = json_decode($request->order);
+    $short = Short::where('user_id',$user->id)->where('stock_ticker',$ar[0])->first();
+    $user->cash -= (($short->shares) * $ar[1]);
+    $user->invest_score += (($short->shares) * $ar[1]) - (($short->shares) * ($short->initial_price));
+    $user->save();
+    $short->delete();
+
+
+
   }
   public function leaderboard(){
     $users = User::all();
